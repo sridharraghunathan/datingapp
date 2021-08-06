ac1a07a088afb80545c500742cbcfbb5c70a5692
Sridhar Raghunathan <mit.sri.20073269@gmail.com>
2021-08-06 11:09:14 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,7 +39,7 @@ namespace API
             {
                 options.UseSqlServer(connectionStringData);
             });
-            
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -59,6 +59,12 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            // Always include CORS policy in same place between routing and the Authorization
+            app.UseCors(policy =>
+             policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  users: any;
+  title = 'client';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe((response) => {
+      this.users = response;
+    });
+  }
+}
